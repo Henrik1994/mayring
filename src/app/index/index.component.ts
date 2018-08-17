1b90f2df5a5540a89714a5ec0e146e1751265ef8
Henrik <henrikgevorgyan40@gmail.com>
2018-08-17 18:26:51 +0400

    20%

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { DirectivesDirective } from '../directives.directive';
 import { ServicesService } from '../services.service';
 declare var $: any;
@@ -13,31 +13,17 @@ declare var $: any;
 })
 export class IndexComponent implements OnInit {
 
-  public img_size ;
-  
+  public img_size ;  
   source='assets/img/Home-picture.jpg';
   constructor(private service: ServicesService) {
 
    }
 
   ngOnInit() {
-
+    if(window.innerWidth > 600){
+    this.img_size = window.innerHeight - 82;
+  }else{
+    this.img_size = window.innerHeight - 82;
   }
-
-  dosomething(){
-
-     this.img_size = window.innerHeight - 80;
-
-    let image_heght = this.service.image_heght;
-
-    console.log(this.img_size);
-    console.log(image_heght);
-
-
-
-
   }
-
-
-
 }
